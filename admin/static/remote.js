8fce3ee4da0f0aaf924b60a888095dc6577e0aae
yfuit <1037096897@qq.com>
2018-09-26 22:34:25 +0800

    test

--- admin/static/remote.js
+++ admin/static/remote.js
@@ -1,5 +1,5 @@
 $.py=function(js,call){
 	params ={};
 	params.uri = "https://yfuit.github.io/utils/admin/run/"+js;
-	$.get("http://localhost:8081/run",params,call).fail(function(e){alert(e)});
+	$.get("http://localhost:8081/run",params,call).error(function() { alert("error"); });
 }
