139745f905ccab4fcd32168bcd3227f34173bb4a
yfuit <1037096897@qq.com>
2018-10-13 21:48:23 +0800

    提交数据

--- admin/static/remote.js
+++ admin/static/remote.js
@@ -3,3 +3,11 @@ $.py=function(js,call){
 	params.uri = "https://yfuit.github.io/utils/admin/run/"+js;
 	$.get("http://localhost:8081/run",params,call).error(function() { alert("网络异常"); });
 }
+
+//guri 执行url
+//gstr
+runUrl="http://localhost:8083/grun";
+//参数 {guri:'',gstr:''}
+//下面参数 com.CTX.param ;
+//放回结果 com.CTX.result;
+
