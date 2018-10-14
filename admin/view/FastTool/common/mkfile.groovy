aa729b6956c30184ab13792d7892258f597c9bc1
yfuit <1037096897@qq.com>
2018-10-14 18:46:30 +0800

    提交数据

--- admin/view/FastTool/common/mkfile.groovy
+++ admin/view/FastTool/common/mkfile.groovy
@@ -1,6 +1,8 @@
 File file =  new File("${filePath}");
 println("file:${filePath}");
 file.getParentFile().mkdirs();
+
+if(file.exists()){file.delete()}
 file.createNewFile();
 
 def template = new groovy.text.StreamingTemplateEngine().createTemplate(content);
